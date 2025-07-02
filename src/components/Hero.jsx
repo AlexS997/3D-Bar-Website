1de2fd8cab7015dfc0f45bd8fa7532a52b82d0aa
AlexS997 <alexandru.solomon@yahoo.com>
2025-07-02 16:14:24 +0200

    Implement About Sectin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,16 +80,17 @@ const Hero = () => {
                         <p>Cool. Crisp. Classic.</p>
                         <p className='subtitle'>Sip the Spirit <br />of Summer</p>
                     </div>
+                    
+                    <div className='view-cocktails'>
+                        <p className='subtitle'>
+                            Every cocktail on our menu is a blend of premium ingredients, 
+                            creative flair, and timeless recipes — designed to delight your
+                            senses.
+                        </p>
+                        <a href="#cocktails">View Cocktails</a>
+                    </div>
                 </div>
 
-                <div className='view-cocktails'>
-                    <p className='subtitle'>
-                        Every cocktail on our menu is a blend of premium ingredients, 
-                        creative flair, and timeless recipes — designed to delight your
-                         senses.
-                    </p>
-                    <a href="#cocktails">View Cocktails</a>
-                </div>
             </div>
         </section>
 
